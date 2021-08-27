be571a46b7ddd748e1e01deb35142cba492f4918
Mike Thomas <mthomas.theadvocate@gmail.com>
2021-08-26 22:23:37 -0700

    JAVA (initial Checkin)

--- java/script.js
+++ java/script.js
@@ -1,50 +1,25 @@
-var saveBtn = document.getElementsByClassName("button");
+// save the localstorage in the save button
+//create the save code
+//var elements = document.querySelectorAll("a");
+//for (var i= 0; i < elements.length; i++) {
+//    elements[i].style.color = "red";
+//}
+var saveBtn = document.getElementById("saveBtn");
 var past = document.querySelector("past");
-var present = document.querySelector("present");
+var present = document.querySelector("present")
 var future = document.querySelector("future");
-var hour = document.getElementById("hour");
-var description = document.getElementById("description")
-var textarea = document.getElementById("textarea");
-var timeblock = document.getElementById("time-block");
-console.log(timeblock);
-var time = document.getElementById("time");
-console.log(time);
+var textarea = document.getElementsByClassName("description")
 
-function time() {
-    var now = moment(moment().format('MMMM Do YYYY, h:mm:ss a'));
-console.log(time);
-    $("time").each(function(i, e) {
-        var time = moment($(e).attr('datetime'));
-    
-        if(now.diff(time, 'days') <= 1) {
-            $(e).html('<span>' + time.from(now) + '</span>');
-        }
-    });
+let saveBtn = window.localStorage;
+document.getElementById("saveBtn").innerHTML = text.length;
+
+function textClick() {
+    description.textContent = saveBtn;
+    console.log(saveBtn);
+}
+description.addEventListner("click", function() {
+
 }
 
 
-
-function setInterval(){
-     var past = document.querySelector("past")
-     if (condition) {
-         
-     } else {
-         
-     }
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//saveBtn.onclick = textarea;
-
+//var saveBtn = JSON.parse(window.localStorage.getItem("button")) || [];
